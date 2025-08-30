b705e5e3ed5ddd0ffb2e5eacae3c9f5f720e9164
RAMKRUSHNA  ZENDE <149296515+ramzende14@users.noreply.github.com>
2025-08-30 08:10:02 +0530

    Delete FILTERSORT directory

--- FILTERSORT/src/main/java/com/resumemaker/filtersort/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.resumemaker.filtersort.Repository;
-
-import com.resumemaker.filtersort.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
